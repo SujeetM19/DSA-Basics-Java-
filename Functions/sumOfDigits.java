2d2680e5d3754b7d50cdc51d1a394e198ea933d4
SujeetM19 <sujeetmishra008@gmail.com>
2022-07-10 19:54:37 +0530

    reorganised

--- Functions/sumOfDigits.java
+++ Functions/sumOfDigits.java
@@ -1,6 +1,7 @@
+package Functions;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+//import java.lang.*;
+//import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
