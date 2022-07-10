62f337a7131bf8d9effdc3c41cb1e6dced2e0ab6
Sujeet Kumar <53964608+SujeetM19@users.noreply.github.com>
2022-07-11 03:29:21 +0530

    Create histogram.java
    raw and brute force of histogram of stars

---
+++ array/histogram.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int a[] = new int[n];
+    
+    for(int i=0; i<a.length; i++){
+        a[i] = scn.nextInt();
+    }
+    
+    int max = a[0];
+    int max_index = 0;
+    for(int i=1; i<a.length; i++){
+        if(a[i]>max){
+            max = a[i];
+            max_index = i;
+        }
+    }
+    
+    //System.out.print(max);
+    //System.out.print(max_index);
+    
+    for(int i=0; i<max; i++){  // j=0 -> 6
+        for(int j=0; j<max_index; j++){   //i=0 -> 2      3 baar
+            if(max-i <= a[j]){             // 7 - 0 < 3  false , true hoga jab 
+                System.out.print("*");
+            }
+            System.out.print("\t");
+        }
+    
+        System.out.print("*\t");
+        
+        for(int j=max_index + 1; j<n; j++){
+            if(max-i <= a[j]){
+                System.out.print("*");
+            }
+            System.out.print("\t");
+        }
+    
+        System.out.println("");
+    
+    }
+    
+    
+ }
+
+}
