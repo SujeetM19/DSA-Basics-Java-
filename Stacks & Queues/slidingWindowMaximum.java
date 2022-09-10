e4442f5ca3aadddaf08238217bf8660a7f619e2e
SujeetM19 <sujeetmishra008@gmail.com>
2022-09-10 22:52:54 +0530

    sliding widow optimised

--- Stacks & Queues/slidingWindowMaximum.java
+++ Stacks & Queues/slidingWindowMaximum.java
@@ -1,16 +1,13 @@
 
-// Sliding window using arrays :-
+Sliding Window Maximum using Deque
 
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing the elements of array a.
+3. You are given a number k, representing the size of window.
+4. You are required to find and print the maximum element in every window of size k.
 
-// Sliding Window Maximum
-// Hard  Prev   Next
-// 1. You are given a number n, representing the size of array a.
-// 2. You are given n numbers, representing the elements of array a.
-// 3. You are given a number k, representing the size of window.
-// 4. You are required to find and print the maximum element in every window of size k.
-
-// e.g.
-// for the array [2 9 3 8 1 7 12 6 14 4 32 0 7 19 8 12 6] and k = 4, the answer is [9 9 8 12 12 14 14 32 32 32 32 19 19 19]
+e.g.
+for the array [2 9 3 8 1 7 12 6 14 4 32 0 7 19 8 12 6] and k = 4, the answer is [9 9 8 12 12 14 14 32 32 32 32 19 19 19]
 
 
 
@@ -30,32 +27,56 @@ public static void main(String[] args) throws Exception {
        a[i] = Integer.parseInt(br.readLine());
     }
     int k = Integer.parseInt(br.readLine());
-
-    // code
-    int [] ans = new int[a.length - k + 1];
-    //sliding window using arrays n2
     
-    // System.out.println(ans.length);
+    int[] ans = new int [n-k+1];
+    Deque<Integer> dq = new ArrayDeque<>();
+    // System.out.println(dq);
     
-    for(int i=0; i<n-k+1; i++){
-        int max = a[i];
-        for(int j=i; j<i+k; j++){
-            if(a[j]>max){
-                max = a[j];
-            }
+    int idx = 0;
+    
+    //for first k elements
+    for(int i=0; i<k; i++){
+        while(dq.size() != 0 && a[i] > a[dq.getLast()]){
+            dq.removeLast();
         }
-        ans[i] = max;
+        
+        dq.addLast(i);
+        // System.out.println(dq.peek());
+    }
+    
+    ans[idx] = a[dq.peek()];
+    idx++;
+    // System.out.println(dq);
+    
+    
+    
+    
+    for(int i=k; i<n; i++){
+        while(dq.size() != 0 && a[i] > a[dq.getLast()]){
+            dq.removeLast();
+        }
+        dq.addLast(i);
+        
+        // System.out.println(dq);
+        
+        
+        if(i-dq.getFirst() >= k){
+            dq.removeFirst();
+        }
+        
+        // System.out.println(dq);
+        ans[idx] = a[dq.getFirst()];
+        idx++;
     }
     
     
-    
-    for(int i=0; i<ans.length; i++){
-        System.out.println(ans[i]);
+    for(int i = 0; i < ans.length; i++){
+      System.out.println(ans[i]);
     }
+
     
  }
 }
-
 
 
 
